a8c23c8e0839ab767ef463478fdcab06e6431532
Natanael Leite <natansl@gmail.com>
2017-11-03 10:25:57 -0200

    Adicionado componente de mensagens ao projeto

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,5 +1,5 @@
-export const MEAT_API = 'http://localhost:3000';
-//export const MEAT_API = 'http://10.12.102.147:3000'
+//export const MEAT_API = 'http://localhost:3000';
+export const MEAT_API = 'http://10.12.102.147:3000'
 //const maskData = [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/];
 
 
