b759eaf17549061c3b83f383cb6435f2377d1237
Tiago Antônio Resende de Oliveira <tiago.oliveira@serpro.gov.br>
2017-12-14 18:28:28 -0200

    ajuste na busca do valor da FCT atual do colaborador e na pontuação quando os papeis são excluídos

--- src/app/avaliacao/avaliacao-form/avaliacao-form.component.ts
+++ src/app/avaliacao/avaliacao-form/avaliacao-form.component.ts
@@ -319,6 +319,7 @@ export class AvaliacaoFormComponent implements OnInit {
     getResetarAtributo(valor) {
         this.PapelAtributo = [];
         this.clearArray();
+        this.somaValores('atributo');
     }
 
     getSomarTecnologia(valor) {
@@ -361,8 +362,13 @@ export class AvaliacaoFormComponent implements OnInit {
             qtdAtributos = qtdAtributos + 1;
             ajusteAtributos = ajusteAtributos + 3;
         });
-        ajusteAtributos = (13 / (ajusteAtributos/3));
-        this.vlrAtributo = this.vlrAtributo * ajusteAtributos;
+        
+        if (ajusteAtributos > 0){
+            ajusteAtributos = (13 / (ajusteAtributos/3));
+            this.vlrAtributo = this.vlrAtributo * ajusteAtributos;
+        }else{
+            this.vlrAtributo = 0.00;
+        }
     }
 
     somaValores(tipo: string) {
@@ -407,9 +413,7 @@ export class AvaliacaoFormComponent implements OnInit {
             }
             case 'fctatual': {
                 // Pontuação FCT Atual
-
-                //this.formAvaliacao.get('vlrFCTatual').setValue(this.vlrAtributo.toFixed(2));
-                this.formAvaliacao.get('vlrFCTatual').setValue(85.39);
+                this.formAvaliacao.get('vlrFCTatual').setValue(this.formAvaliacao.get('colaborador').value.pontuacao.toFixed(2));
                 
                 // Referencia FCT Atual | %
                 this.formAvaliacao.get('referenciaFctAtual').setValue(this.formAvaliacao.get('colaborador').value.referenciaFct);
